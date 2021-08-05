21f2789ecdae1dc6c2eb8afb3bbc0cf62f321590
Jonas Gehrlein <jonas@web3.foundation>
2021-08-05 08:32:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Kraken (Market)Sell Script
+
+## Disclaimer
+By using this script, you give it access to your Binance account. I do not take any responsibility of bugs which could cause loss of funds.
+
+## What it does
+
+This simple script will sell a user-specified amount of DOT tokens for EUR as a market-sell-order. Before doing so, it first shows your DOT balance and the current DOT price.
+
+## Installation
+
+### Set your API keys as environmental variables
+* Get your Kraken API keys.
+* Set your API keys as environmental variables. Use `API_KEY_KRAKEN` for your public key and `API_SEC_KRAKEN` for your secret key.
+* Install the krakenex python library with `pip install python-krakenex`.
+
+## Running
+Open the folder containing the script with your terminal and type:
+`python3 kraken_sell.py`
---
+++ kraken_sell.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import time
+import os
+import requests
+import urllib.parse
+import hashlib
+import hmac
+import base64
+
+# Read Kraken API key and secret stored in environment variables
+api_url = "https://api.kraken.com"
+api_key = os.environ['API_KEY_KRAKEN']
+api_sec = os.environ['API_SEC_KRAKEN']
+
+asset = 'KSM'
+fiat = 'EUR'
+pair = asset + fiat
+
+
+def get_kraken_signature(urlpath, data, secret):
+
+    postdata = urllib.parse.urlencode(data)
+    encoded = (str(data['nonce']) + postdata).encode()
+    message = urlpath.encode() + hashlib.sha256(encoded).digest()
+
+    mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+    sigdigest = base64.b64encode(mac.digest())
+    return sigdigest.decode()
+
+def makeRequest(uri_path, data, api_key, api_sec):
+    headers = {}
+    headers['API-Key'] = api_key
+    # get_kraken_signature() as defined in the 'Authentication' section
+    headers['API-Sign'] = get_kraken_signature(uri_path, data, api_sec)             
+    req = requests.post((api_url + uri_path), headers=headers, data=data)
+    return req
+
+# Construct the request and print the result
+token_balance_request = makeRequest('/0/private/TradeBalance', {
+    "nonce": str(int(1000*time.time())),
+    "asset": asset
+}, api_key, api_sec)
+
+
+dot_ticker = requests.get('https://api.kraken.com/0/public/Ticker?pair=DOTEUR')
+dot_price = dot_ticker.json()['result']['DOTEUR']['a'][0]
+
+ksm_ticker = requests.get('https://api.kraken.com/0/public/Ticker?pair=KSMEUR')
+ksm_price = ksm_ticker.json()['result']['KSMEUR']['a'][0]
+
+balances = makeRequest('/0/private/Balance', {
+    "nonce": str(int(1000*time.time()))
+}, api_key, api_sec)
+
+dot_balance = float(balances.json()['result']['DOT'])
+ksm_balance = float(balances.json()['result']['KSM'])
+
+print('KSM is currently trading at ' + str(ksm_price) + '.' + ' You currently have ' + str(ksm_balance) + ' available')
+print('DOT is currently trading at ' + str(dot_price) + '.' + ' You currently have ' + str(dot_balance) + ' available')
+
+
+while True:
+    try:
+        sell_asset = int(input('Which asset do you want to trade? Insert 1 for KSM and 2 for DOT '))
+        if (sell_asset == 1 or sell_asset == 2):
+            break
+        raise Exception()
+    except:
+        print('Please input valid option')
+
+while True:
+    try:
+        sell_amount = float(input('And how much of the token do you want to sell? '))
+        if (sell_asset == 1):
+            if(sell_amount <= ksm_balance and sell_amount >= 0.02):
+                pair = 'KSM' + fiat
+                break
+        else:
+            if(sell_amount <= dot_balance and sell_amount >= 0.02):
+                pair = 'DOT' + fiat
+                break
+        raise Exception()
+    except:
+        print('You cannot sell more than you have or the minimum sell amount is 0.02.')
+
+
+sell = makeRequest('/0/private/AddOrder', {
+    "nonce": str(int(1000*time.time())),
+    "ordertype": "market",
+    "type": "sell",
+    "volume": str(sell_amount),
+    "pair": pair,
+}, api_key, api_sec)
+
+print(sell.json())
